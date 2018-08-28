ab2d71a1371a70de4d7b8a5679399b56a98216d4
Jose Miller <32144156+Jose714@users.noreply.github.com>
2018-08-27 19:01:34 -0500

    first commit

---
+++ app/src/main/java/com/example/lds/listas/modelo/Estudiante.java
@@ -0,0 +1,44 @@
+package com.example.lds.listas.modelo;
+
+public class Estudiante {
+
+    String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    String apellido;
+    String sexo;
+    String carrera;
+}
+
+
